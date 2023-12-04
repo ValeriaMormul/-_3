d6f684cdea1ff3eef573d99118ebdc88a1ed883e
Valeriia Mormul <152411242+ValeriaMormul@users.noreply.github.com>
2023-12-05 00:11:07 +0200

    Добавьте файлы проекта.

---
+++ Цикл_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число для таблицы умножения: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Таблица умножения для числа {number}:");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            int result = number * i;
+            Console.WriteLine($"{number} * {i} = {result}");
+        }
+    }
+}
